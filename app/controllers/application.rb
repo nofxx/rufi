959c76dc510b08182eac688cdf6db53347cd0c32
Marcos Augusto <x@nofxx.com>
2008-07-09 12:02:51 -0300

    my 2 cents on sheiks flexigrid method

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   before_filter :login_required 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '4008a843b7f2120eb3e66893063f1ba9'
+  protect_from_forgery :secret => '4008a843b7f2120eb3e66893063f1ba9'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
@@ -32,19 +32,19 @@ class ApplicationController < ActionController::Base
   	start = ((page - 1) * rp).to_i
   	query = '%'+query+'%'
   	
-  	if query == '%%'
-  		@collection = model_class.all :select => select, :order => sortname+' '+sortorder, :limit => rp, :offset => start
+  	if query == '%%'                                 
+  	  puts select
+  		@collection = model_class.all :select => select.join(','), :order => sortname+' '+sortorder, :limit => rp, :offset => start
   		count = model_class.count
   	else
-			@collection = model_class.all :select => select, :order => sortname+' '+sortorder, :limit => rp, :offset => start, :conditions => [qtype+' like ?', query]			
+			@collection = model_class.all :select => select.join(','), :order => sortname+' '+sortorder, :limit => rp, :offset => start, :conditions => [qtype+' like ?', query]			
   		count = model_class.count :conditions => [qtype+' like ?', query]
   	end
   	
   	output = {}
   	output[:page] = page
   	output[:total] = count
-  	output[:rows] = @collection.collect { |c| {:cell => select.collect {|s| c[s]} }}
-  	
+  	output[:rows] = @collection.map { |c| { :cell => select.map { |s| c[s] }}}
   	output
   
   end
