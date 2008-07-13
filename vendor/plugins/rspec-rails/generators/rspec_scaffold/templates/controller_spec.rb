c7fae5ea0f67ce56857abddd923c83818158c72a
Marcos Augusto <x@nofxx.com>
2008-07-13 15:54:24 -0300

    up some plugins

--- vendor/plugins/rspec-rails/generators/rspec_scaffold/templates/controller_spec.rb
+++ vendor/plugins/rspec-rails/generators/rspec_scaffold/templates/controller_spec.rb
@@ -3,139 +3,56 @@ require File.expand_path(File.dirname(__FILE__) + '<%= '/..' * class_nesting_dep
 describe <%= controller_class_name %>Controller do
 
   def mock_<%= file_name %>(stubs={})
-    stubs = {
-      :save => true,
-      :update_attributes => true,
-      :destroy => true,
-      :to_xml => ''
-    }.merge(stubs)
     @mock_<%= file_name %> ||= mock_model(<%= class_name %>, stubs)
   end
-
-  describe "responding to GET /<%= table_name %>" do
-
-    it "should succeed" do
-      <%= class_name %>.stub!(:find)
-      get :index
-      response.should be_success
-    end
-
-    it "should render the 'index' template" do
-      <%= class_name %>.stub!(:find)
-      get :index
-      response.should render_template('index')
-    end
   
-    it "should find all <%= table_name %>" do
-      <%= class_name %>.should_receive(:find).with(:all)
-      get :index
-    end
-  
-    it "should assign the found <%= table_name %> for the view" do
-      <%= class_name %>.should_receive(:find).and_return([mock_<%= file_name %>])
+  describe "responding to GET index" do
+
+    it "should expose all <%= table_name.pluralize %> as @<%= table_name.pluralize %>" do
+      <%= class_name %>.should_receive(:find).with(:all).and_return([mock_<%= file_name %>])
       get :index
       assigns[:<%= table_name %>].should == [mock_<%= file_name %>]
     end
 
-  end
-
-  describe "responding to GET /<%= table_name %>.xml" do
-
-    before(:each) do
-      request.env["HTTP_ACCEPT"] = "application/xml"
-    end
+    describe "with mime type of xml" do
   
-    it "should succeed" do
-      <%= class_name %>.stub!(:find).and_return([])
-      get :index
-      response.should be_success
-    end
-
-    it "should find all <%= table_name %>" do
-      <%= class_name %>.should_receive(:find).with(:all).and_return([])
-      get :index
-    end
-  
-    it "should render the found <%= table_name %> as xml" do
-      <%= class_name %>.should_receive(:find).and_return(<%= file_name.pluralize %> = mock("Array of <%= class_name.pluralize %>"))
-      <%= file_name.pluralize %>.should_receive(:to_xml).and_return("generated XML")
-      get :index
-      response.body.should == "generated XML"
-    end
+      it "should render all <%= table_name.pluralize %> as xml" do
+        request.env["HTTP_ACCEPT"] = "application/xml"
+        <%= class_name %>.should_receive(:find).with(:all).and_return(<%= file_name.pluralize %> = mock("Array of <%= class_name.pluralize %>"))
+        <%= file_name.pluralize %>.should_receive(:to_xml).and_return("generated XML")
+        get :index
+        response.body.should == "generated XML"
+      end
     
+    end
+
   end
 
-  describe "responding to GET /<%= table_name %>/1" do
+  describe "responding to GET show" do
 
-    it "should succeed" do
-      <%= class_name %>.stub!(:find)
-      get :show, :id => "1"
-      response.should be_success
-    end
-  
-    it "should render the 'show' template" do
-      <%= class_name %>.stub!(:find)
-      get :show, :id => "1"
-      response.should render_template('show')
-    end
-  
-    it "should find the requested <%= file_name %>" do
-      <%= class_name %>.should_receive(:find).with("37")
+    it "should expose the requested <%= file_name %> as @<%= file_name %>" do
+      <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
       get :show, :id => "37"
-    end
-  
-    it "should assign the found <%= file_name %> for the view" do
-      <%= class_name %>.should_receive(:find).and_return(mock_<%= file_name %>)
-      get :show, :id => "1"
       assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
     end
     
+    describe "with mime type of xml" do
+
+      it "should render the requested <%= file_name %> as xml" do
+        request.env["HTTP_ACCEPT"] = "application/xml"
+        <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
+        mock_<%= file_name %>.should_receive(:to_xml).and_return("generated XML")
+        get :show, :id => "37"
+        response.body.should == "generated XML"
+      end
+
+    end
+    
   end
 
-  describe "responding to GET /<%= table_name %>/1.xml" do
-
-    before(:each) do
-      request.env["HTTP_ACCEPT"] = "application/xml"
-    end
+  describe "responding to GET new" do
   
-    it "should succeed" do
-      <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>)
-      get :show, :id => "1"
-      response.should be_success
-    end
-  
-    it "should find the <%= file_name %> requested" do
-      <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
-      get :show, :id => "37"
-    end
-  
-    it "should render the found <%= file_name %> as xml" do
-      <%= class_name %>.should_receive(:find).and_return(mock_<%= file_name %>)
-      mock_<%= file_name %>.should_receive(:to_xml).and_return("generated XML")
-      get :show, :id => "1"
-      response.body.should == "generated XML"
-    end
-
-  end
-
-  describe "responding to GET /<%= table_name %>/new" do
-
-    it "should succeed" do
-      get :new
-      response.should be_success
-    end
-  
-    it "should render the 'new' template" do
-      get :new
-      response.should render_template('new')
-    end
-  
-    it "should create a new <%= file_name %>" do
-      <%= class_name %>.should_receive(:new)
-      get :new
-    end
-  
-    it "should assign the new <%= file_name %> for the view" do
+    it "should expose a new <%= file_name %> as @<%= file_name %>" do
       <%= class_name %>.should_receive(:new).and_return(mock_<%= file_name %>)
       get :new
       assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
@@ -143,66 +60,39 @@ describe <%= controller_class_name %>Controller do
 
   end
 
-  describe "responding to GET /<%= table_name %>/1/edit" do
-
-    it "should succeed" do
-      <%= class_name %>.stub!(:find)
-      get :edit, :id => "1"
-      response.should be_success
-    end
+  describe "responding to GET edit" do
   
-    it "should render the 'edit' template" do
-      <%= class_name %>.stub!(:find)
-      get :edit, :id => "1"
-      response.should render_template('edit')
-    end
-  
-    it "should find the requested <%= file_name %>" do
-      <%= class_name %>.should_receive(:find).with("37")
+    it "should expose the requested <%= file_name %> as @<%= file_name %>" do
+      <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
       get :edit, :id => "37"
-    end
-  
-    it "should assign the found <%= class_name %> for the view" do
-      <%= class_name %>.should_receive(:find).and_return(mock_<%= file_name %>)
-      get :edit, :id => "1"
       assigns[:<%= file_name %>].should equal(mock_<%= file_name %>)
     end
 
   end
 
-  describe "responding to POST /<%= table_name %>" do
+  describe "responding to POST create" do
 
-    describe "with successful save" do
-  
-      it "should create a new <%= file_name %>" do
-        <%= class_name %>.should_receive(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>)
+    describe "with valid params" do
+      
+      it "should expose a newly created <%= file_name %> as @<%= file_name %>" do
+        <%= class_name %>.should_receive(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => true))
         post :create, :<%= file_name %> => {:these => 'params'}
-      end
-
-      it "should assign the created <%= file_name %> for the view" do
-        <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>)
-        post :create, :<%= file_name %> => {}
         assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
       end
 
       it "should redirect to the created <%= file_name %>" do
-        <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>)
+        <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>(:save => true))
         post :create, :<%= file_name %> => {}
         response.should redirect_to(<%= table_name.singularize %>_url(mock_<%= file_name %>))
       end
       
     end
     
-    describe "with failed save" do
+    describe "with invalid params" do
 
-      it "should create a new <%= file_name %>" do
-        <%= class_name %>.should_receive(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => false))
+      it "should expose a newly created but unsaved <%= file_name %> as @<%= file_name %>" do
+        <%= class_name %>.stub!(:new).with({'these' => 'params'}).and_return(mock_<%= file_name %>(:save => false))
         post :create, :<%= file_name %> => {:these => 'params'}
-      end
-
-      it "should assign the invalid <%= file_name %> for the view" do
-        <%= class_name %>.stub!(:new).and_return(mock_<%= file_name %>(:save => false))
-        post :create, :<%= file_name %> => {}
         assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
       end
 
@@ -216,49 +106,39 @@ describe <%= controller_class_name %>Controller do
     
   end
 
-  describe "responding to PUT /<%= table_name %>/1" do
+  describe "responding to PUT udpate" do
 
-    describe "with successful update" do
+    describe "with valid params" do
 
-      it "should find the requested <%= file_name %>" do
+      it "should update the requested <%= file_name %>" do
         <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
-        put :update, :id => "37"
-      end
-
-      it "should update the found <%= file_name %>" do
-        <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>)
         mock_<%= file_name %>.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => "1", :<%= file_name %> => {:these => 'params'}
+        put :update, :id => "37", :<%= file_name %> => {:these => 'params'}
       end
 
-      it "should assign the found <%= file_name %> for the view" do
-        <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>)
+      it "should expose the requested <%= file_name %> as @<%= file_name %>" do
+        <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => true))
         put :update, :id => "1"
         assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
       end
 
       it "should redirect to the <%= file_name %>" do
-        <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>)
+        <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => true))
         put :update, :id => "1"
         response.should redirect_to(<%= table_name.singularize %>_url(mock_<%= file_name %>))
       end
 
     end
     
-    describe "with failed update" do
+    describe "with invalid params" do
 
-      it "should find the requested <%= file_name %>" do
-        <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>(:update_attributes => false))
-        put :update, :id => "37"
-      end
-
-      it "should update the found <%= file_name %>" do
-        <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>)
+      it "should update the requested <%= file_name %>" do
+        <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
         mock_<%= file_name %>.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => "1", :<%= file_name %> => {:these => 'params'}
+        put :update, :id => "37", :<%= file_name %> => {:these => 'params'}
       end
 
-      it "should assign the found <%= file_name %> for the view" do
+      it "should expose the <%= file_name %> as @<%= file_name %>" do
         <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:update_attributes => false))
         put :update, :id => "1"
         assigns(:<%= file_name %>).should equal(mock_<%= file_name %>)
@@ -274,21 +154,16 @@ describe <%= controller_class_name %>Controller do
 
   end
 
-  describe "responding to DELETE /<%= table_name %>/1" do
+  describe "responding to DELETE destroy" do
 
-    it "should find the <%= file_name %> requested" do
+    it "should destroy the requested <%= file_name %>" do
       <%= class_name %>.should_receive(:find).with("37").and_return(mock_<%= file_name %>)
+      mock_<%= file_name %>.should_receive(:destroy)
       delete :destroy, :id => "37"
     end
   
-    it "should call destroy on the found <%= file_name %>" do
-      <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>)
-      mock_<%= file_name %>.should_receive(:destroy)
-      delete :destroy, :id => "1"
-    end
-  
     it "should redirect to the <%= table_name %> list" do
-      <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>)
+      <%= class_name %>.stub!(:find).and_return(mock_<%= file_name %>(:destroy => true))
       delete :destroy, :id => "1"
       response.should redirect_to(<%= table_name %>_url)
     end
--- vendor/plugins/rspec-rails/spec_resources/controllers/controller_spec_controller.rb
+++ vendor/plugins/rspec-rails/spec_resources/controllers/controller_spec_controller.rb
@@ -1,4 +1,12 @@
 class ControllerSpecController < ActionController::Base
+  before_filter :raise_error, :only => :action_with_skipped_before_filter
+  
+  def raise_error
+    raise "from a before filter"
+  end
+  
+  skip_before_filter :raise_error
+  
   if ['edge','2.0.0'].include?(ENV['RSPEC_RAILS_VERSION'])
     set_view_path [File.join(File.dirname(__FILE__), "..", "views")]
   else
@@ -33,7 +41,7 @@ class ControllerSpecController < ActionController::Base
   end
       
   def action_which_sets_cookie
-    cookies[:cookie_key] = params[:value]
+    cookies['cookie_key'] = params[:value]
     render :text => ""
   end
       
@@ -73,6 +81,10 @@ class ControllerSpecController < ActionController::Base
       page.replace :bottom, 'replace_me',
                             :partial => 'non_existent_partial'
     end
+  end
+  
+  def action_with_skipped_before_filter
+    render :text => ""
   end
 end
 
